ababbb2637046f0984f9572907ffd182b1216c07
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-12-26 01:28:52 -0600

    creando una escena

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import Scene from "./components/Scene";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Esta es mi applicacion
-        </p>
-      </header>
-    </div>
+    <Scene/>
   );
 }
 
---
+++ src/components/Scene.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Scene(){
+    return (
+        <div className="Contenedor3D" style={{width:"100%", height:"100vh"}}>
+            <h1> hola mundo</h1>
+        </div>
+    )
+}
