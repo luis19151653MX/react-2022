c80132bfabfb982db4160d41b1b200cf76b21329
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-12-26 00:46:01 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Esta es mi app
+          Esta es mi applicacion
         </p>
       </header>
     </div>
